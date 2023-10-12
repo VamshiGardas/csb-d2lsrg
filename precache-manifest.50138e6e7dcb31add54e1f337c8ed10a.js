a6d462eedc27b84d23f8000393a683b65b2e0413
Code Sandbox <hello@codesandbox.io>
2023-10-12 08:24:41 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.50138e6e7dcb31add54e1f337c8ed10a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b00cdc9432d921e5266be5f08c3e380e",
+    "url": "https://vamshigardas.github.io/csb-d2lsrg/index.html"
+  },
+  {
+    "revision": "ca32429f7eb8ceee628d",
+    "url": "https://vamshigardas.github.io/csb-d2lsrg/static/js/2.62409189.chunk.js"
+  },
+  {
+    "revision": "098ba44b1f467799fe9e",
+    "url": "https://vamshigardas.github.io/csb-d2lsrg/static/js/main.b2812e96.chunk.js"
+  },
+  {
+    "revision": "f5c0b2b1d55a66e90fbe",
+    "url": "https://vamshigardas.github.io/csb-d2lsrg/static/js/runtime-main.b6c415ba.js"
+  }
+]);
